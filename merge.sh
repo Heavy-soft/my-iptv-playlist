e380ef8a9768ccce2645c4c5285ec5368ed2ff9d
Heavy-soft <87676725+Heavy-soft@users.noreply.github.com>
2026-01-08 15:14:15 +0100

    Create merge.sh

---
+++ merge.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+# IPTV-ORG LANGUAGE PLAYLISTS (AUTO UPDATED)
+ARABIC="https://iptv-org.github.io/iptv/languages/ara.m3u"
+FRENCH="https://iptv-org.github.io/iptv/languages/fra.m3u"
+
+OUT="combined.m3u"
+
+echo "#EXTM3U" > $OUT
+
+curl -s "$ARABIC" | tail -n +2 >> $OUT
+curl -s "$FRENCH" | tail -n +2 >> $OUT
